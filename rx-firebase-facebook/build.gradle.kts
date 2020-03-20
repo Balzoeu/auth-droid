d8555e15e8e1666b73ddbe61acd59e79e6388ed3
Giacomo Parisi <giacomoparisi95@gmail.com>
2020-03-20 19:41:19 +0100

    Release 0.2-beta04

--- rx-firebase-facebook/build.gradle.kts
+++ rx-firebase-facebook/build.gradle.kts
@@ -101,7 +101,6 @@ publishing {
 
             pom.withXml {
                 asNode().apply {
-                    appendNode("packaging", "jar")
                     appendNode("description", Library.pomDescription)
                     appendNode("name", "auth-droid")
                     appendNode("url", Library.pomUrl)
