21653a02ce197dc1cd6bf9c47283bb61edbf9b61
Giacomo Parisi <giacomoparisi95@gmail.com>
2020-03-20 19:39:22 +0100

    Release 0.2-beta03

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -2,6 +2,6 @@ object AndroidConfig {
     const val min_sdk = 21
     const val target_sdk = 28
     const val compile_sdk = 28
-    const val version_code = 6
-    const val version_name = "0.2-beta02"
+    const val version_code = 1
+    const val version_name = "0.2-beta03"
 }
