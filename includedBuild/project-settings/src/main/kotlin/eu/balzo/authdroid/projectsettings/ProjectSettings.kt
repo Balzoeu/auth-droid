14a1b39969964b68678ceda883dda3479ef2cc5d
Giacomo Parisi <giacomoparisi95@gmail.com>
2023-03-30 14:51:22 +0200

    Release 1.0.0-beta03

--- includedBuild/project-settings/src/main/kotlin/eu/balzo/authdroid/projectsettings/ProjectSettings.kt
+++ includedBuild/project-settings/src/main/kotlin/eu/balzo/authdroid/projectsettings/ProjectSettings.kt
@@ -5,5 +5,5 @@ object ProjectSettings {
     const val target_sdk = 30
     const val compile_sdk = 31
     const val version_code = 1
-    const val version_name = "1.0.0-beta02"
+    const val version_name = "1.0.0-beta03"
 }
