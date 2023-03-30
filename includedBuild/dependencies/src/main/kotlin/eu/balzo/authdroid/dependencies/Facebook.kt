ff414224a6bc3ea3d046cbc29daea60454753aed
GIOVANNI VITIELLO <giovanni.vitiello10@gmail.com>
2023-03-30 13:01:52 +0200

    update facebook deps

--- includedBuild/dependencies/src/main/kotlin/eu/balzo/authdroid/dependencies/Facebook.kt
+++ includedBuild/dependencies/src/main/kotlin/eu/balzo/authdroid/dependencies/Facebook.kt
@@ -2,8 +2,8 @@ package eu.balzo.authdroid.dependencies
 
 object Facebook {
 
-    const val core = "com.facebook.android:facebook-core:13.1.0"
+    const val core = "com.facebook.android:facebook-core:16.0.1"
 
-    const val login = "com.facebook.android:facebook-login:13.1.0"
+    const val login = "com.facebook.android:facebook-login:16.0.1"
 
 }
